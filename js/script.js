c92d05423d0cc0c875d7890bd867811e93d9b7a2
mahmud035 <mahamudulhasan4148@gmail.com>
2022-02-17 09:38:04 +0600

    comment added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//-----------Handle Calculate button event-------------
 document
   .getElementById('calculate-button')
   .addEventListener('click', function () {
@@ -11,7 +12,7 @@ document
     const clotheInput = document.getElementById('clothe-value');
     const clotheInputValue = parseFloat(clotheInput.value);
 
-    // Error Handle: input isn't number
+    // -----Error Handle: input isn't number-----
     if (
       isNaN(incomeInputValue) ||
       isNaN(foodInputValue) ||
@@ -20,7 +21,7 @@ document
     ) {
       return alert('Please input valid amount of money in number format');
     }
-    // Error Handle: negative input
+    // -----Error Handle: negative input-----
     if (
       incomeInputValue < 0 ||
       foodInputValue < 0 ||
@@ -35,7 +36,7 @@ document
 
     const balance = document.getElementById('balance');
     const balanceAmount = incomeInputValue - totalExpense;
-    // Error Handle: total expenses is higher than income
+    // -----Error Handle: total expenses is higher than income-----
     if (totalExpense > incomeInputValue) {
       return alert(`Your expenses cannot exceed your income`);
     } else {
@@ -44,9 +45,9 @@ document
     }
   });
 
-//Handle Save button event
+//--------------Handle Save button event----------------
 document.getElementById('save-button').addEventListener('click', function () {
-  // remaining balance
+  // -----remaining balance-----
   const incomeInput = document.getElementById('income-value');
   const incomeInputValue = parseFloat(incomeInput.value);
   const foodInput = document.getElementById('food-value');
@@ -64,19 +65,19 @@ document.getElementById('save-button').addEventListener('click', function () {
   const balanceAmount = incomeInputValue - totalExpense;
   balance.innerText = balanceAmount;
 
-  // saving amount
+  // -----saving amount-----
   const saveInput = document.getElementById('saving-percent');
   const saveInputValue = parseFloat(saveInput.value);
 
   const savingAmount = incomeInputValue * (saveInputValue / 100);
   const amount = document.getElementById('saving-amount');
-  // Error Handle: saving amount greater than balance
+  // -----Error Handle: saving amount greater than balance-----
   if (savingAmount > balanceAmount) {
     return alert(`You can't save more than your balance`);
   } else {
     amount.innerText = savingAmount;
   }
-  // remaining balance
+  // -----remaining balance-----
   const remainingBalance = balanceAmount - savingAmount;
   document.getElementById('remaining-balance').innerText = remainingBalance;
 });
